c380704190a5ae60712b288fa3a00bf5ca3c7318
rosycg <christa@mflhq.com>
2022-02-23 12:30:12 -0600

    adds PeerEval java maven project

---
+++ src/main/java/peerEval/PeerEval.java
@@ -0,0 +1,25 @@
+package peerEval;
+
+/***********************************
+*	Authors: Christa Greenwood, Garrett Powell, Megan Skeen
+*	Class: CS 375.01 - Spring 2022
+*	Project - Peer Evaluations
+*
+*
+*	How to compile/build:
+*		mvn compile
+*	
+*	How to run/execute:
+*		java -cp target\classes peerEval.PeerEval
+*
+*	How to test:
+*		mvn test
+*
+************************************/
+public class PeerEval
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/peerEval/PeerEvalTest.java
@@ -0,0 +1,20 @@
+package peerEval;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class PeerEvalTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
