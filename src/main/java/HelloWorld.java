695e220c28a091bb7f7c9798da3e3a528fb7befe
rosycg <christa@mflhq.com>
2022-02-23 12:08:46 -0600

    adds maven project java files - HelloWorld for testing

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,15 @@
+/***********************************
+*	Authors: Christa Greenwood, Garrett Powell, Megan Skeen
+*	Class: CS 375.01 - Spring 2022
+*
+*	Project - Peer Evaluations
+*
+************************************/
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, World!");
+	}	
+}
