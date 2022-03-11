ba99346154024ea2ec0be78efe4907ead4830453
Garrett Powell <gbp18a@acu.edu>
2022-03-10 20:15:16 -0600

    1 test added

--- src/test/java/peerEval/PeerEvalTest.java
+++ src/test/java/peerEval/PeerEvalTest.java
@@ -1,9 +1,13 @@
 package peerEval;
-import java.io.*;  
-
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotEquals;
+import org.junit.*;
 
-import org.junit.Test;
+import java.io.File; 
+import java.io.IOException;
+import java.util.*;
+import java.io.*;
 
 /**
  * Unit test for simple App.
@@ -11,11 +15,15 @@ import org.junit.Test;
 public class PeerEvalTest 
 {
     @Test
-    public void shouldAnswerWithTrue()
+    public void ReadCSV()
     {
         PeerEval test = new PeerEval();
-
         InputStream is = test.loadFile("sampleCSV.csv");
-        test.printFile(is);
+        try{  
+            assertNotEquals(is, null);
+                }
+        catch(Exception e){
+            System.out.println("Read CSV failed");
+        }
     }
 }
