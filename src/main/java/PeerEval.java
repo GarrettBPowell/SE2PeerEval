57a91e3299095821ed94b0ca70fca671c719a6a6
rosycg <christa@mflhq.com>
2022-04-27 01:52:33 -0500

    adds admin functionality to html

--- src/main/java/PeerEval.java
+++ src/main/java/PeerEval.java
@@ -436,6 +436,8 @@ public class PeerEval
         Scanner sin = new Scanner(System.in);
         System.out.print("\nAdmin View?\nAdmin ID: ");
         String classID = sin.nextLine();
+        System.out.print("\nWhat should the output file be named?\nFilename:");
+        String htmlFilename = sin.nextLine();
 
         try{
         Double alltimeAvg = 0.0;
@@ -451,14 +453,14 @@ public class PeerEval
             }
 
             String[] stats = new String[1];
-            stats[0] = alltimeAvg.toString();
+            stats[0] = "Value Average: " + alltimeAvg.toString();
            
             //String queryString = "Select * from v_response where student2 = '" + classID;
-
+            queryString = "Select * from response";
             //
             //System.out.println("Select * from v_response where student2 = '2' AND evalid = '999';");
             //printResultSet(query(queryString));
-            //createHTMLResult(query(queryString));
+            createHTMLResult(query(queryString), htmlFilename, 2, false, stats);
             
         }catch(Exception e){
                 System.out.print("Failed printAdminView");
@@ -972,6 +974,9 @@ public class PeerEval
         switch (reportType){
             case 1:
                 htmlHeader = "Student View";
+                break;
+            case 2:
+                htmlHeader = "Admin View";
                 break;
         }
 
